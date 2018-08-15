66a79eee11e178de2782e6d5e378b5f06b5fe102
Kendra Kelly and Maly Phongsavanh <kendra@>
2018-08-15 16:37:20 -0700

    add in js to recognize first vowel and break word from there

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // business logic
-var consanants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'y', 'z', 'qu'];
+var consanants = ['qu', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'y', 'z', ];
 var vowels = ["a", "e", "i", "o", "u"];
 var ay = "ay";
 var way = "way";
@@ -12,30 +12,51 @@ var way = "way";
 $(document).ready(function() {
  $('#input-form').submit(function(event) {
    event.preventDefault();
+   var firstLetter = inputSentence.charAt(0);
+   var secondLetter = inputSentence.charAt(1);
    var inputSentence = $("#inputSentence").val();
+    for (var h = 0; h < inputSentence.length; 1++) {
+      if (vowels.includes(inputSentence.charAt(h))) {
+        var firstVowel = h;
+        break;
+      }
+    }
 
+    var endOfword = inputSentence.slice(firstVowel, inputSentence.length);
+    var begOfword = inputSentence.slice(0, firstVowel);
 
   for (var i = 0; i < 1; i++) {
     for (var j = 0; j < vowels.length; j++)
     if (inputSentence.length === 1 && inputSentence === vowels[j]) {
       alert(inputSentence + ay);
     }
-    else if (inputSentence.charAt(0) === vowels[j] )
+    else if (inputSentence.charAt(0) === vowels[j]) {
       alert(inputSentence + way);
-}
-
-  for (var i = 0; i < 1; i++) {
+    }
+    }
+  for (var m = 0; m < 1; m++) {
     for (var k = 0; k < consanants.length; k++)
     if (inputSentence.charAt(0) === consanants[k]) {
-      var shift = inputSentence.charAt(0);
-      var removed = inputSentence.slice(1);
-      alert(removed + shift + ay);
-    } else if (inputSentence.charAt(0) && inputSentence.charAt(1) === consanants[k]) {
-      
+      var shift1 = inputSentence.charAt(0);
+      var removed1 = inputSentence.slice(1);
+      alert(removed1 + shift1 + ay);
+    else if (consanants.includes(firstLetter)) {
+      var wholeFinalWord = endOfword + begOfword + ay;
+      return combinedWord;
+      else {
+        return inputSentence;
+      }
     }
-  }
+    }
+
+      // var shift2 = (inputSentence.charAt(0) + inputSentence.charAt(1));
+      // var removed2 = inputSentence.slice(2);
+      // alert(removed2 + shift2 + ay);
+
+
 
   });
+
 });
 
 
