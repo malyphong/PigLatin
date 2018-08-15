7822190cb391026272c18e68a7e1ec58389dee9e
Kendra Kelly and Maly Phongsavanh <maly@>
2018-08-15 14:06:54 -0700

    add js to add ay to single vowels

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // business logic
 var consanants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'y', 'z', 'qu'];
-var vowels = ['a', 'e', 'i', 'o', 'u'];
+var vowels = ["a", "e", "i", "o", "u"];
+var ay = "ay";
 
-// if ()
 
 
 
@@ -13,13 +13,40 @@ $(document).ready(function() {
  $('#input-form').submit(function(event) {
    event.preventDefault();
    var inputSentence = $("#inputSentence").val();
-   var slicedArray = inputSentence.split(" ");
-
-   var notAphabet = !consanants && !vowels;
-
-   console.log(notAphabet);
 
 
 
- });
+  // if (inputSentence.length === 1) {
+  //   // $('#translated').text(inputSentence + ay);
+  //   alert(inputSentence + "ay");
+  // }
+  for (var i = 0; i <inputSentence.length; i++) {
+    for (var j = 0; j <vowels.length; j++)
+    if (inputSentence.length === 1 && inputSentence === vowels[j]) {
+      // $('#translated').text(inputSentence + ay);
+      alert(inputSentence + ay);
+  }
+}
+  });
 });
+
+
+
+
+
+
+
+
+
+
+  //   var notAlphabet = slicedArray[i];
+  //
+
+     // if (inputSentence === vowels) {
+     //   alert("vowel!!");
+     // } else {
+     //   alert("this is not a vowel");
+     // }
+     // }
+
+// var slicedArray = inputSentence.split(" ");
