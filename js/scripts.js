d94c1c77c1e35165cd22865dff2a82f212786dd0
Kendra Kelly and Maly Phongsavanh <maly@>
2018-08-15 11:42:35 -0700

    add in user interface logic for the form and first variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
-$(document).ready(function) {
+// business logic
+var consanants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'y', 'z', 'qu'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+// if ()
+
+
+
+
+
+
+// user interface
+$(document).ready(function() {
  $('#input-form').submit(function(event) {
    event.preventDefault();
-
-
-
-
+   var inputSentence = $("#inputSentence").val();
+console.log(inputSentence);
  });
 });
