ae5e089ebca0d4075809308f2a63238175d86b50
Kendra Kelly, Maly Phongsavanh and Nikki Wong <maly@>
2018-08-16 13:33:31 -0700

    add jquery functionality for 2 letter words and numbers

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 #### _Application to translate regular English input into Pig Latin, 8/15/18_
 
-#### By _**Kendra Kelly & Maly Phongsavanh**_
+#### By _**Kendra Kelly, Nikki Wong & Maly Phongsavanh**_
 
 ## Description
 
@@ -62,4 +62,4 @@ _{Tell me about the languages and tools you used to create this app. Assume that
 
 *{Determine the license under which this application can be used.  See below for more details on licensing.}*
 
-Copyright (c) 2016 **_{List of contributors or company name}_**
+Copyright (c) 2018 **_{List of contributors or company name}_**
--- js/scripts.js
+++ js/scripts.js
@@ -4,47 +4,37 @@ var vowels = ["a", "e", "i", "o", "u"];
 var ay = "ay";
 var way = "way";
 var firstWord;
-var pigLating = [];
+var pigLatin = [];
 
 var translatedSentence = function(inputString) {
-  console.log(inputString);
   var firstLetter = inputString.charAt(0);
   var secondLetter = inputString.charAt(1);
-
 
   for (var i = 0; i < inputString.length; i +=1) {
     if (vowels.includes(inputString.charAt(i))) {
       var firstVowel = (inputString.charAt(i));
       break;
-
     }
   }
+
   var endOfword = inputString.slice(i, inputString.length);
   var begOfword = inputString.slice(0, i);
   var quEndOfword = inputString.slice(2, inputString.length);
   var quBegOfword = inputString.slice(0, 2);
 
-
-
-  if (inputString.charAt(0) === vowels[i]) {
-    // console.log(inputString + way);
+  if (inputString.charAt(0) === "a" || inputString.charAt(0) === "e" || inputString.charAt(0) === "i" || inputString.charAt(0) === "o" || inputString.charAt(0) === "u") {
+    var wholeFinalWord = inputString + way;
   } else if ((inputString.length > 1) && (firstVowel === 0)) {
     var wholeFinalWord = inputString.concat(way);
-    // console.log(firstVowel);
   } else if (inputString.charAt(0) === "q" && inputString.charAt(1) === "u") {
     var wholeFinalWord = quEndOfword + quBegOfword + ay;
-    // console.log(wholeFinalWord);
   } else if (consanants.includes(firstLetter)) {
     var wholeFinalWord = endOfword + begOfword + ay;
-    // console.log(wholeFinalWord);
   } else {
-   // alert(inputString);
+   var wholeFinalWord = inputString;
   }
-
   return wholeFinalWord;
 }
-
-
 
 // user interface
 $(document).ready(function() {
@@ -52,40 +42,12 @@ $(document).ready(function() {
    event.preventDefault();
     var inputSentence = $("#inputSentence").val();
     inputSentence = inputSentence.split(" ");
-    // console.log(inputSentence);
 
     inputSentence.forEach(function(inputString) {
-      console.log(inputString);
-      // var inputString = [];
       var result = translatedSentence(inputString);
-      console.log(result);
+      pigLatin.push(result);
+      var finalResult = pigLatin.join(" ");
+      $("#translated").text(finalResult);
     })
-
-
-    // var result = translatedSentence(inputSentence);
-
-    // $('.translated').text(result);
-
   });
 });
-
-
-
-
-
-
-
-
-
-
-  //   var notAlphabet = slicedArray[i];
-  //
-
-     // if (inputSentence === vowels) {
-     //   alert("vowel!!");
-     // } else {
-     //   alert("this is not a vowel");
-     // }
-     // }
-
-// var slicedArray = inputSentence.split(" ");
